823fef1ae7d74d4f3fa53e3bd48aa4d35fc1fc5c
threestrikes <wontez@protonmail.com>
2019-04-23 23:16:39 -0400

    updates

--- src/Functions.hs
+++ src/Functions.hs
@@ -5,13 +5,15 @@ module Functions
     ) where
 
 import System.IO
+import Data.List
 
 promptLine :: String -> IO String 
 promptLine prompt = do
     putStr prompt 
     getLine
 
-
+toCommaSeparatedString :: [String] -> String
+toCommaSeparatedString = intercalate ","
 
 -- Creates roster ID
 rosterId2 = do  -- figure out how to add the correct type signature here
@@ -21,12 +23,30 @@ rosterId2 = do  -- figure out how to add the correct type signature here
             putStrLn "How many Students?";
             numberStudents <- getLine
             let numberStudentsInt = (read numberStudents :: Int )
+
 --            numberStudents <- promptLine "How many Students? ";
             let className = classNumber ++ "-" ++ classYear
             let fileName = className ++ ".txt"
 
-            let accountStandard = "username,password,firstname,lastname,email,course1,group1,type1"  
+            let username = classNumber ++ "-stu"
+            let password = "Student1"
+            let firstname = "Student "
+            let lastname = "Class " ++ className
+            let email = "Student" ++ className ++"@cc.mil"
+            let course1 = "Common Core"
+            let group1 = lastname 
+            let type1 = "1"
+
+            let line1 = "username,password,firstname,lastname,email,course1,group1,type1"  
+            let studentLine = [username,password,firstname
+                              ,lastname,email,course1,group1
+                              ,type1
+                              ]
+
+            let concatStudentLine = toCommaSeparatedString studentLine
+
             let studentStandard = "\n" ++ classNumber ++ "-stu" ++ "1" ++ ",Student" ++ "1" ++ ",Student " ++"1,"++ "Class " ++ className ++ "," ++ "1" ++ "Student" ++ className ++ "@cc.mil,Common Core,Class " ++ className ++ ",1" 
+            let studentStan = "\n" ++ classNumber ++ "-stu" 
             let studentStandard2 = replicate numberStudentsInt (studentStandard)
             let studentStandard3 = concat studentStandard2 
             
@@ -36,7 +56,7 @@ rosterId2 = do  -- figure out how to add the correct type signature here
             let userEmail = userLogin ++ "@commoncore.com"
             let userCourse = "Common Core"
          
-            let accountList = accountStandard ++ studentStandard3 ++ "\n" ++ instructorStandard
+            let accountList = line1 ++ concatStudentLine ++ "\n" ++ instructorStandard
             
             writeFile fileName accountList;
 
